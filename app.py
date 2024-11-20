edfd476fd37d2344b808c3e26551f842057d488f
Hikmalaf <147985507+Hikmalaf@users.noreply.github.com>
2024-11-20 20:22:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -90,4 +90,5 @@ def predict():
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))  # Gunakan $PORT atau default 8080
+    app.run(host='0.0.0.0', port=port)
